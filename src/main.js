023eda57e57bed0b24eb5c2594373c0c72190bdc
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2021-12-05 00:35:40 +0100

    feat: use pinia to manage store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import './assets/tailwind.css'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app')
