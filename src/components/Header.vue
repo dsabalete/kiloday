aed7dcd245e21d0f8ef657ff11561185e1987059
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2021-12-05 00:37:14 +0100

    feat: create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="text-center text-green-200">
+    <h1 class="text-4xl font-bold">KiloDay</h1>
+    {{ currentDate }}
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  setup() {
+    const currentDate = new Date().toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric'
+    })
+    return {
+      currentDate
+    }
+  }
+}
+</script>
