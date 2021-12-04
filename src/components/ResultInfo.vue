ce51c94f90c0d9561d629f3420690caf609bab78
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2021-12-05 00:36:14 +0100

    feat: create ResultInfo component

---
+++ src/components/ResultInfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <section v-if="isValid">
+    <h2 class="section-title">
+      Your date of birth: <br />
+      {{ birthDateFmt }}
+    </h2>
+    <div>Your age: {{ days }} days</div>
+    <div>({{ age }} years and {{ daysFromBirthday }} days)</div>
+    <br />
+    <div>Next kiloday to celebrate: {{ nextKilodateToCelebrate }} days</div>
+    <div>Next kiloday: {{ nextKilodate }}</div>
+  </section>
+</template>
+
+<script>
+import { useBirthdayStore } from '@/stores/birthday'
+
+export default {
+  name: 'ResultInfo',
+  setup() {
+    const store = useBirthdayStore()
+
+    store.$patch({
+      birthDate: {
+        day: 10,
+        month: 1,
+        year: 1973
+      }
+    })
+
+    const {
+      isValid,
+      birthDateFmt,
+      days,
+      age,
+      daysFromBirthday,
+      nextKilodateToCelebrate,
+      nextKilodate
+    } = store
+
+    return {
+      isValid,
+      birthDateFmt,
+      days,
+      age,
+      daysFromBirthday,
+      nextKilodateToCelebrate,
+      nextKilodate
+    }
+  }
+}
+</script>
